24458988dfc5967495db0cd0e7efd9cdd39514aa
Chagrilled <Andrew.n64@hotmail.com>
2019-10-22 01:55:21 +0100

    Reduce divs

--- app/components/gallery/FilterBar.js
+++ app/components/gallery/FilterBar.js
@@ -12,11 +12,9 @@ export default class FilterBar extends Component<Props> {
       <span style={{'width': '50%'}}>
         <div className="input-group">
           <input type="text" className="form-control"/>
-          <div className="input-group-append">
-            <div className="input-group-text">
+          <div className="input-group-append input-group-text">
               <input style={{'margin':'0 5px 0px 0'}} type="checkbox"/>
               Analysed
-            </div>
           </div>
         </div>
       </span>
