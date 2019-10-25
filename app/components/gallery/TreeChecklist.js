6a7409cda4e6c0e5745554bf189d47c8ee95e26a
Daniel Cordell <danielbc@hotmail.co.uk>
2019-10-25 13:51:42 +0100

    Fixed Icons & checking folders will also check folders with same name

--- app/components/gallery/TreeChecklist.js
+++ app/components/gallery/TreeChecklist.js
@@ -23,7 +23,7 @@ export default class TreeChecklist extends Component<Props> {
         if (!nodes) return [];
         return nodes.map((item, i) => {
             return { 
-                value: item.name, 
+                value: item.path, 
                 label: item.name,
                 children: getNodes(item.children)
             }
@@ -46,6 +46,18 @@ export default class TreeChecklist extends Component<Props> {
             expanded={this.state.expanded}
             onCheck={checked => this.setState({ checked })}
             onExpand={expanded => this.setState({ expanded })}
+            icons={{
+              check: <i class="far fa-check-square"/>,
+              uncheck: <i class="far fa-square"/>,
+              halfCheck: <i class="far fa-minus-square"/>,
+              expandClose: <i class="fas fa-chevron-right"/>,
+              expandOpen: <i class="fas fa-chevron-down"/>,
+              expandAll: <i class="fas fa-plus-square"/>,
+              collapseAll: <i class="fas fa-minus-square"/>,
+              parentClose: <i class="far fa-folder"/>,
+              parentOpen: <i class="far fa-folder-open"/>,
+              leaf: <i class="far fa-folder"/>,
+            }}
         />
       </div>
     );
