c2f142db589e5dd4100bd1a1baba59052a45172f
Chagrilled <Andrew.n64@hotmail.com>
2019-10-25 23:27:32 +0100

    Put folders into unstyled accordions. I think they're a bit shit.

--- app/components/gallery/FolderView.js
+++ app/components/gallery/FolderView.js
@@ -1,19 +1,26 @@
 // @flow
 import React, { Component } from 'react';
 import RemoveButton from '../containers/RemoveButtonContainer'
+import { Accordion, Card } from 'react-bootstrap';
 
 type Props = {};
 
 export default class FolderView extends Component<Props> {
   props: Props;
-
+  
   render() {
+    
     const { folder } = this.props;
     return (
-      <div data-tid="container">
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey={this.props.eventKey}>
           Hello from {folder}!
           <RemoveButton path={folder}/>
-      </div>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={this.props.eventKey}>
+          <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
     );
   }
 }
--- app/components/gallery/GalleryView.js
+++ app/components/gallery/GalleryView.js
@@ -2,6 +2,9 @@
 import React, { Component } from 'react';
 import styles from './GalleryView.scss';
 import FolderView from './FolderView';
+import { Accordion } from 'react-bootstrap';
+import styled from 'styled-components';
+
 type Props = {};
 
 export default class GalleryView extends Component<Props> {
@@ -9,18 +12,23 @@ export default class GalleryView extends Component<Props> {
 
   render() {
     const { folders } = this.props;
-
+    const StyledAccordion = styled(Accordion)` && {
+        color: black;
+    }`
+    
     return (
       <div className={styles.container} data-tid="container">
         <div className={styles.title}>
           <h1>Open Folders</h1>
         </div>
+        <StyledAccordion defaultActiveKey="0">
         {
           folders.map((item, i) => {
             if (folders.length > 0)
-              return <FolderView key={item} folder={item}/>;
+              return <FolderView key={item} folder={item} eventKey={i} />;
           })
         }
+        </StyledAccordion>
       </div>
     );
   }
