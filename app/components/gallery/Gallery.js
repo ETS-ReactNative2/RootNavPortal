2c5d20cde232146d676b8945e3f43fcc2aae8a82
Daniel Cordell <danielbc@hotmail.co.uk>
2019-10-25 15:26:36 +0100

    Fix file reading

--- app/components/gallery/Gallery.js
+++ app/components/gallery/Gallery.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import styles from './Gallery.css';
 import TopBar from './TopBar';
 import GalleryView from '../containers/GalleryViewContainer';
-import { existsSync, readFileSync } from 'fs';
+import { existsSync, readFile } from 'fs';
 import { APPHOME, CONFIG } from '../../constants/globals';
 
 type Props = {};
@@ -13,19 +13,23 @@ export default class Gallery extends Component<Props> {
 
   render() {
 
-    console.log("Rendering galelry")
+    console.log("Rendering gallery")
     if (!this.props.hasReadConfig) 
     {
       console.log("Not read")
-      if (existsSync(APPHOME + CONFIG))
-        readFileSync(APPHOME + CONFIG, (err, data) => { if (err)
-           console.log(err); 
-           console.log("hello I've read"); 
-           
-           console.log(data); 
-           this.props.importConfig(JSON.parse(data))
-       }); 
-       else console.log("doesn't exist: " + APPHOME+CONFIG);          
+      console.log(APPHOME + CONFIG)
+      if (existsSync(APPHOME + CONFIG)) {
+        readFile(APPHOME + CONFIG, "utf8", (err, data) => {
+          console.log("HMMMMM"); 
+          if (err)
+            console.log(err); 
+          console.log("hello I've read"); 
+          
+          console.log(data); 
+          this.props.importConfig(JSON.parse(data))
+        }); 
+      }
+      else console.log("doesn't exist: " + APPHOME+CONFIG);     
     }
 
     return (
