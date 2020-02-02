6dd503f5d050a740e8195cf851437f84a7fa186e
Daniel Cordell <danielbc@hotmail.co.uk>
2020-02-02 01:21:14 +0000

    Added case-insensitivity to regex

--- app/constants/globals.js
+++ app/constants/globals.js
@@ -8,10 +8,9 @@ export const API_HOST = 'http://localhost';
 export const API_PORT = 8841;
 export const API_PATH = API_HOST + ":" + API_PORT;
 
-export const IMAGE_EXTS = ['jpg', 'jpeg', 'jpe', 'jfif', 'jif', 'png', 'tif'];
+export const IMAGE_EXTS = ['jpg', 'jpeg', 'jpe', 'jfif', 'jif', 'png', 'tif', 'tiff'];
 export const DATA_EXTS = ['rsml'];
 export const ALL_EXTS = IMAGE_EXTS.concat(DATA_EXTS);
 
-export const IMAGE_EXTS_REGEX = new RegExp(IMAGE_EXTS.join("|"))
-export const ALL_EXTS_REGEX = new RegExp(`(.+)\.(${IMAGE_EXTS.join("|")})$`);
-console.log(ALL_EXTS_REGEX);
+export const IMAGE_EXTS_REGEX = new RegExp(IMAGE_EXTS.join("|"), 'i') // Regex for all image extensions (i - case insensitive)
+export const ALL_EXTS_REGEX = new RegExp(`(.+)\.(${IMAGE_EXTS.join("|")})$`, 'i'); // Regex for all files (i - case insensitive)
