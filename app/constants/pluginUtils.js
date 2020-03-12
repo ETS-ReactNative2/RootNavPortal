93615497e8098e87cb038ce8a47ebb7ad0e33f52
Chagrilled <Andrew.n64@hotmail.com>
2020-03-12 17:58:07 +0000

    Emergence distance plugin + utils

--- app/constants/pluginUtils.js
+++ app/constants/pluginUtils.js
@@ -5,15 +5,43 @@ module.exports = {
         {
             let p1 = points[i];
             let p2 = points[i+1];
-            distance += Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
+            distance += pointDistance(p1, p2);
         }
         return distance;
     },
     getPlantID: line => line.id.match(/([0-9]+)/)[1],
-    getPlantPrimaryID: line => line.id.match(/([0-9]+)-([0-9]+)/),
-    getAllIDs: line => line.id.match(/([0-9]+)-([0-9]+).?([0-9]+)?/),
+    getPlantPrimaryID: line => line.id.match(/(?<plantID>[0-9]+)-(?<primaryID>[0-9]+)/).groups,
+    getAllIDs: line => line.id.match(/(?<plantID>[0-9]+)-(?<primaryID>[0-9]+).?(?<lateralID>[0-9]+)?/).groups,
     addToAverage: (current, newVal, frame) => (current * (frame-1) + newVal) / frame,
     getParentOfLateral: (lateral, polylines) => polylines.find(line => line.id == lateral.id.match(/(\d+\-\d+)/)[1]),
     getTag: json => json.rsml[0].metadata[0]['file-key'][0]["$t"],
-    isMultiplePlants: json => json.rsml[0].scene[0].plant.length > 1
+    isMultiplePlants: json => json.rsml[0].scene[0].plant.length > 1,
+    getAngle: (anchor, point) => Math.atan2(anchor.y - point.y, anchor.x - point.x) * 180 / Math.PI,
+    getNearestPoint: (points, point) => {
+        let closestPoint, closestDistance = Infinity;
+
+        points.forEach(coord => {
+            let distance = pointDistance(point, coord);
+            if (distance < closestDistance) 
+            {
+                closestDistance = distance;
+                closestPoint = coord;
+            }
+        });
+        return closestPoint;
+    },
+    getDistanceUntilPoint: (points, breakPoint) => {
+        let distance = 0;
+        for (let i = 0; i < points.length - 1; i++)
+        {
+            let p1 = points[i];
+            let p2 = points[i+1];
+            distance += pointDistance(p1, p2);
+            if (p2.x == breakPoint.x && p2.y == breakPoint.y) break;
+        }
+        return distance;
+    },
+    pointDistance
 }
+
+pointDistance = (p1, p2) => Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
